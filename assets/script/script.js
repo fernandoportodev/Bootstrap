cf763b2702ae8be1d46e9a5712dbb972c41c70a8
Fernando Porto <fernandogomes143@gmail.com>
2022-03-22 15:56:22 -0300

    @fernandoportodev Task: Criando aplicação Web com Bootstrap

---
+++ assets/script/script.js
@@ -0,0 +1,61 @@
+const nome = document.querySelector('#nome')
+const email = document.querySelector('#email')
+const assunto = document.querySelector('#assunto')
+let contador = document.querySelector('#contador')
+contador.innerHTML = '0 / 100'
+
+let nomeOk = false
+let emailOk = false
+let assuntoOk = false
+
+function validaNome(){
+    const txtNome = document.querySelector('#txtNome')
+
+    if(nome.value.length >= 3) {
+        txtNome.style.color = '#26733a';
+        txtNome.innerHTML = 'Nome válido ✔';
+        nomeOk = true;
+    } else {
+        txtNome.style.color = '#732626';
+        txtNome.innerHTML = 'Nome inválido ❌ - Insira pelo menos 3 caracteres';
+    }
+}
+
+function validarEmail() {
+    let txtEmail = document.querySelector('#txtEmail');
+
+    let regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+
+    if(email.value.match(regex)){
+        txtEmail.innerHTML = 'E-mail válido ✔';
+        txtEmail.style.color = '#26733a';
+        emailOk = true;
+    } else {
+        txtEmail.innerHTML = 'E-mail inválido - preciso de um @ e de um . ❌';
+        txtEmail.style.color = '#732626';
+    }
+}
+
+function validarAssunto(){
+    let txtAssunto = document.querySelector('#txtAssunto');
+
+    contador.innerHTML = `${assunto.value.length} / 100`
+
+if (assunto.value.length > 100) {
+    txtAssunto.innerHTML =
+    'Texto é muito grande, digite no máximo 100 caracteres ❌';
+    txtAssunto.style.color = '#732626';
+    txtAssunto.style.display = 'block';
+} else {
+    txtAssunto.style.display = 'none';
+    assuntoOk = true;
+}
+}
+
+    function enviar() {
+        if (nomeOk == true && emailOk == true) {
+        alert('Formulário enviado com sucesso!');
+        } else {
+        alert('Preencha o formulário corretamente antes de enviar...');
+        }
+    }    
